7e82692643452db68b2bda213037c742f948f942
varsha-2530 <vb8607518@gmaail.com>
2025-07-30 23:57:36 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,9 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174',
+    'https://tumhara-frontend-link.onrender.com' 
+  ],
   credentials: true
 }));
 
